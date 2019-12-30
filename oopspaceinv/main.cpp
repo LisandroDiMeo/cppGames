f580578e422c9de3cc1e45d30c381cd1f672bf48
LisandroDiMeo <ddmlisandro22@gmail.com>
2019-12-30 00:24:33 -0300

    nave ya tiene movimiento

--- oopspaceinv/main.cpp
+++ oopspaceinv/main.cpp
@@ -23,6 +23,62 @@ enum Direction{
     RIGHT
 };
 
+struct XWing_body{
+
+    char body[18] = {
+        '|',' ',' ',' ',' ','|',
+        '|','_','/','\\','_','|',
+        '|',' ','\\','/',' ','|'
+        };
+    int dimx = 3;
+    int dimy = 6;
+};
+
+
+class XWing{
+
+    public:
+        XWing(){
+            xPos = 0;
+            shipStatus = 100;
+        }
+        void changeDirection(Direction dir){
+            switch(dir){
+                case LEFT:
+                    xPos--;
+                    break;
+                case RIGHT:
+                    xPos++;
+                    break;
+                case UP:
+                    Shoot();
+                default:
+                    break;
+            }
+        }
+
+        void Shoot(){
+            
+        }
+
+        void draw(int d){
+            for(int i = 0; i < xwing.dimx * xwing.dimy; i++){
+                if (i % 6 == 0) d++;
+                char *a = new char(xwing.body[i]);
+                mvprintw(d,1 + (i % 6) + xPos,a);
+                free(a);
+            }
+        }
+        int sizeX() const { return xwing.dimx; };
+        int sizeY() const { return xwing.dimy; };
+        int getPos() const { return xPos; };
+    private:
+        int xPos;
+        int shipStatus;
+        XWing_body xwing;
+
+        
+};
 
 class Game{
     public:
@@ -37,6 +93,7 @@ class Game{
         while(!gameOver){
             updateGameScreen();
             input();
+            xwing.changeDirection(dir);
         }
         getch();
         endwin();
@@ -45,11 +102,14 @@ class Game{
     private:
         Display display;
         Direction dir;
+        XWing xwing;
         bool gameOver;
         int score;
 
         void Setup(){
             initscr();
+            start_color();
+            init_pair(1, COLOR_GREEN, COLOR_BLACK);
             clear();
             noecho();
             cbreak();
@@ -58,14 +118,27 @@ class Game{
 
         void updateGameScreen(){
             clear();
-            for(int i = 0; i < display.width + 2; i++)
+            for(int i = 0; i < display.width + 2; i++){
+                attron(COLOR_PAIR(1));
                 mvprintw(0,i,"*");
+                refresh();
+            }
+                
             for(int i = 0; i < display.height + 2; i++){
                 for(int j = 0; j < display.width + 2; j++){
-                    if( i == 0 | i == display.height + 1)
+                    if( i == 0 | i == display.height + 1){
                         mvprintw(i,j,"*");
-                    else if( j == 0 | j == display.height + 1 )
+                    }
+                        
+                    else if( j == 0 | j == display.width + 1 ){
                         mvprintw(i,j,"*");
+                    }
+                        
+                    else if(i == display.height - xwing.sizeY() &&  j == 3 ){
+                        // Fijarse en donde dibujar la nave
+                        xwing.draw(display.height - xwing.sizeY() + 2);
+                    }
+                        
                 }
             }
             mvprintw(display.height + 3, 0, "Score %d",score);
@@ -97,7 +170,7 @@ class Game{
 
 
 int main(){
-    Game game(20,20);
+    Game game(70,20);
     game.startGame();
     return 0;
 }
